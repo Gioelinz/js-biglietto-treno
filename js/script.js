ede4b1a8c8b6b82cb50a7d49f9595f08869e59f8
Gioelinz <gioelerunza.gr@gmail.com>
2021-12-21 18:17:25 +0100

    add conditions

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ L'output del prezzo finale va messo fuori in forma umana
 /*
 1- chiedere età e km da percorrere
 2- calcolare prezzo moltiplicando per 0.21€ al km
-3- condizioni se minorenni o se over 65.
+3- calcolo percentuali
+4- condizioni se minorenni o se over 65.
 */
 
 // #1 Chiedo al cliente età e km
@@ -25,4 +26,24 @@ console.log('userAge:', userAge);
 
 // 2# calcolo prezzo moltiplicando km * 0.21€km
 
-console.log(userKM * 0.21)
+const finalKMPrice = userKM * 0.21
+
+console.log('finalKmPrice: ', finalKMPrice)
+
+// #3 calcolo percentuali
+
+const off20 = finalKMPrice - finalKMPrice * 20 / 100;
+console.log('off20: ', off20);
+
+const off40 = finalKMPrice - finalKMPrice * 40 / 100;
+console.log('off40: ', off40);
+
+// 4# condizioni
+
+if (userAge <= 18) {
+    console.log("Hai ricevuto uno sconto del 20%, il prezzo migliore per te che sei minorenne  è ", off20)
+} else if (userAge >= 65) {
+    console.log("Hai ricevuto uno sconto del 40%, il prezzo migliore per te che sei over 65  è ", off40)
+} else {
+    console.log("Il prezzo finale per le tue necessità è: ", finalKMPrice)
+}
